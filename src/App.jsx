862f2d31bb59d639fc22ca3fbe1c5d4a79519d5b
Lounas <lounasidjr@gmail.com>
2025-08-19 19:20:02 +0100

    configure

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 import './App.css'
 import Navbar from './component/Navbar';
 function App() {
-
+     
   return (
     <>
    <Navbar/>
- <div className="text-center mt-10 px-4">
-  <h1 className="text-4xl lg:text-5xl font-bold leading-snug">
+ <div className="relative text-center mt-10 px-4 top-[80px]">
+  <h1 className=" text-4xl lg:text-5xl font-bold leading-snug font-sans">
     Prenez votre <span className="text-[#0FDCBC]">rendez-vous</span> <br />
     <span className="text-[#0FDCBC]">médical</span> en quelques clics !
   </h1>
-  <p className="mt-4 text-gray-600 text-lg">
+  <p className="mt-4 text-gray-600 text-lg font-sans">
     Trouvez votre clinique et réservez facilement en ligne, <br />
     où que vous soyez.
   </p>
