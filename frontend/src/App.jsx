5b48e0c7a3f381d3c261f3b5274642d296126d2f
Lounas <lounasidjr@gmail.com>
2026-01-07 15:07:39 +0100

    restructure file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+// import { useState } from 'react'
+import './App.css';
+import "./index.css"; 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import Navbar from './component/Navbar';
+import Search from './component/Search';
+import OurStat from './component/OurStat'
+import HowItWorks from './component/HowItWorks';
+import MySwiper from './component/MySwiper'
+import Faqs from './component/Faqs';
+import Footer from './component/Footer';
+
+
+function App() {
+     
+  return (
+    <>
+<div>
+   <Navbar/>
+   <div className="relative text-center mt-10 px-4 top-[62px]">
+  <h1 className=" text-4xl lg:text-5xl font-bold leading-snug font-sans">
+    Prenez votre <span className="text-[#0FDCBC]">rendez-vous</span> <br />
+    <span className="text-[#0FDCBC]">médical</span> en quelques clics !
+  </h1>
+  <p className="mt-4 text-gray-600 text-lg font-sans">
+    Trouvez votre clinique et réservez facilement en ligne, <br />
+    où que vous soyez.
+  </p>
+  <Search />
+   </div>
+   <div><OurStat/></div>
+   <HowItWorks/>
+   <MySwiper/>
+   <Faqs/>
+   <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/component/HowItWorks.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Vector from '../assets/Vector.svg'
+import date from '../assets/Date.svg'
+import searchclinique from '../assets/searchClinique.svg'
+
+
+
+const HowItWorks = () => {
+  
+
+  return (
+    <section className="py-16">
+      <div className="max-w-5xl mx-auto text-center">
+        <h2 className="text-4xl font-bold mb-12">Comment ça marche ?</h2>
+
+        <div className="grid grid-rows-3 gap-8">
+        <div className="bg-white p-6  hover:shadow-lg transition flex gap-4" >
+             <div className="flex">
+              <p className="flex items-center justify-center rounded-full bg-[#0FDCBC] w-[54px] h-[54px] text-white font-bold">1</p>
+              <div className="flex-1">
+              <h3 className="text-xl font-bold mx">Recherchez votre clinique <br /> ou cabinet médical</h3>
+              <p className="text-gray-600">Entrez simplement le nom de la clinique ou du <br/>  médecin que vous recherchez et choisissez  <br/> celui qui vous convient le mieux.
+</p>
+              </div>
+              </div>
+              <img src={searchclinique} alt='Clinique' className="w-[198px] h-[heigh] mx-auto mb-4" />
+
+              </div>
+              <div className="bg-white p-6  hover:shadow-lg transition flex gap-4" >
+              <img src={date} alt='Calendar' className="w-[198px] h-[heigh] mx-auto mb-4" />
+             <div className="flex">
+              <div className="flex-1">
+              <h3 className="text-xl font-bold mx">Prenez rendez-vous en ligne</h3>
+              <p className="text-gray-600"> Sélectionnez la date et l’heure qui vous <br/> conviennent. Finalisez votre demande de rendez- <br/> vous en ligne et recevez une notification dès que <br/> celui-ci est confirmé par l’établissement.</p>
+              </div>
+              <p className="flex items-center justify-center rounded-full bg-[#0FDCBC] w-[54px] h-[54px] text-white font-bold">2</p>
+
+              </div>
+              </div>
+
+            <div className="bg-white p-6  hover:shadow-lg transition flex gap-4" >
+             <div className="flex">
+              <p className="flex items-center justify-center rounded-full bg-[#0FDCBC] w-[54px] h-[54px] text-white font-bold">3</p>
+              <div className="flex-1">
+              <h3 className="text-xl font-bold mx">Confirmez votre présence</h3>
+              <p className="text-gray-600">Recevez un rappel automatique trois jours avant <br /> votre rendez-vous. Confirmez votre présence en <br />un clic pour garantir votre créneau et permettre à <br /> l’établissement de mieux organiser son planning.</p>
+              </div>
+              </div>
+              <img src={Vector} alt='confirmation' className="w-[198px] h-[heigh] mx-auto mb-4" />
+
+              </div>
+        </div>
+      </div>
+      <div class="line"></div>
+    </section>
+  
+  );
+};
+
+export default HowItWorks;
